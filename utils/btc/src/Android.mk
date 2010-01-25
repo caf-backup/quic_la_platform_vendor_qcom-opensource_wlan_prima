f6984e4627328fccba66d746fdaf5f658f060c10
Tim Monahan-Mitchell <tmonahan@quicinc.com>
2010-01-25 11:41:20 -0600

    wlan: libra: Add Android debug output, fixed NULL memcpy

--- utils/btc/src/Android.mk
+++ utils/btc/src/Android.mk
@@ -17,4 +17,12 @@ LOCAL_C_INCLUDES:= $(BTC_INCLUDE_PATH)
 
 LOCAL_PRELINK_MODULE := false
 
+# needed for Android LOG functions
+LOCAL_SHARED_LIBRARIES := \
+	libcutils
+
+ifeq ($(TARGET_BUILD_TYPE),debug)
+LOCAL_CFLAGS += -DBTC_DEBUG
+endif
+
 include $(BUILD_SHARED_LIBRARY)
