d1c43403ae8715e08016e4d3b07a3f3b2c571e86
Sridhar Parasuram <sridharp@quicinc.com>
2010-02-05 11:28:00 -0700

    Add dependency for ACP.
    There occured a build failure where this module was being compiled
    even before the ACP binary was built. This change makes sure that
    ACP is present before the necessary files are copied using ACP.
    
    Change-Id: I5837fc89b206c59756317c9791d89f48e0bab808

--- CORE/HDD/src/Android.mk
+++ CORE/HDD/src/Android.mk
@@ -7,6 +7,7 @@ PRODUCT_COPY_FILES += vendor/qcom-proprietary/wlan/firmware_bin/qcom_nv.bin:syst
 PRODUCT_COPY_FILES += vendor/qcom-proprietary/wlan/firmware_bin/cfg.dat:system/etc/firmware/wlan/cfg.dat
 PRODUCT_COPY_FILES += vendor/qcom-proprietary/wlan/firmware_bin/qcom_cfg.ini:system/etc/firmware/wlan/qcom_cfg.ini
 
+ACP_BINARY_OUT := $(HOST_OUT)/bin/acp
 MAKE_MODULES_FOLDER := out/target/product/$(TARGET_PRODUCT)/system/lib/modules
 WLAN_OUT := vendor/qcom-proprietary/wlan/CORE/HDD/src/libra.o
 WLAN_FTM_OUT := vendor/qcom-proprietary/wlan/ftm/CORE/HDD/src/libra_ftm.o
@@ -32,20 +33,20 @@ ALL_PREBUILT += $(file)
 $(MAKE_MODULES_FOLDER) :
 	mkdir -p $(MAKE_MODULES_FOLDER)
 
-$(WLAN_OUT):  $(KERNEL_OUT) $(KERNEL_CONFIG) $(TARGET_PREBUILT_KERNEL)
+$(WLAN_OUT): $(KERNEL_OUT) $(KERNEL_CONFIG) $(TARGET_PREBUILT_KERNEL)
 	$(MAKE) -C kernel M=../vendor/qcom-proprietary/wlan/CORE/HDD/src O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- 
 
-$(WLAN_FTM_OUT):  $(KERNEL_OUT) $(KERNEL_CONFIG) $(TARGET_PREBUILT_KERNEL) $(WLAN_OUT)
+$(WLAN_FTM_OUT): $(KERNEL_OUT) $(KERNEL_CONFIG) $(TARGET_PREBUILT_KERNEL) $(WLAN_OUT)
 	$(MAKE) -C kernel M=../vendor/qcom-proprietary/wlan/ftm/CORE/HDD/src O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- BUILD_FTM_DRIVER=1
 	
 # Package only incase of 7x30 target
-$(WLAN_PRODUCT_OUT): $(WLAN_OUT) $(MAKE_MODULES_FOLDER)
+$(WLAN_PRODUCT_OUT): $(ACP_BINARY_OUT) $(WLAN_OUT) $(MAKE_MODULES_FOLDER)
 	$(ACP) -f out/target/product/$(TARGET_PRODUCT)/obj/vendor/qcom-proprietary/wlan/CORE/HDD/src/libra.ko $(WLAN_PRODUCT_OUT)
 
-$(WLAN_FTM_PRODUCT_OUT): $(WLAN_PRODUCT_OUT) $(WLAN_FTM_OUT) $(MAKE_MODULES_FOLDER)
+$(WLAN_FTM_PRODUCT_OUT): $(ACP_BINARY_OUT) $(WLAN_PRODUCT_OUT) $(WLAN_FTM_OUT) $(MAKE_MODULES_FOLDER)
 	$(ACP) -f out/target/product/$(TARGET_PRODUCT)/obj/vendor/qcom-proprietary/wlan/ftm/CORE/HDD/src/libra_ftm.ko $(WLAN_FTM_PRODUCT_OUT)
 	
-$(WLAN_LIBRA_SDIOIF_OUT): $(WLAN_OUT) $(MAKE_MODULES_FOLDER)
+$(WLAN_LIBRA_SDIOIF_OUT): $(ACP_BINARY_OUT) $(WLAN_OUT) $(MAKE_MODULES_FOLDER)
 	$(ACP) -f $(KERNEL_OUT)/drivers/net/wireless/libra/librasdioif.ko $(WLAN_LIBRA_SDIOIF_OUT)
 
 all: $(WLAN_OUT) $(WLAN_FTM_OUT) $(WLAN_PRODUCT_OUT) $(WLAN_FTM_PRODUCT_OUT) $(WLAN_LIBRA_SDIOIF_OUT) $(MAKE_SYMBOLIC_LINK)
