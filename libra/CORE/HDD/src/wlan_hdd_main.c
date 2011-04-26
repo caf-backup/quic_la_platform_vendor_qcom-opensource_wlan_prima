8f8307556c7bfac8ac1e33f109e3eb6e6e50253e
Swaroop Golti <sgolti@qualcomm.com>
2011-04-26 14:33:44 +0530

    Fixed MCastbcast filter=3 is not working in some corner cases CR fixed: 278624
    Change-Id: I47b449ee93b107d425de22434f7adafdb33f5c40

--- libra/CORE/HDD/src/wlan_hdd_main.c
+++ libra/CORE/HDD/src/wlan_hdd_main.c
@@ -135,6 +135,8 @@ struct notifier_block hdd_netdev_notifier = {
 #ifdef CONFIG_HAS_EARLYSUSPEND
 extern void register_wlan_suspend(void);
 extern void unregister_wlan_suspend(void);
+void hdd_unregister_mcast_bcast_filter(hdd_adapter_t *pAdapter);
+void hdd_register_mcast_bcast_filter(hdd_adapter_t *pAdapter);
 #endif
 
 #ifdef WLAN_SOFTAP_FEATURE
@@ -741,6 +743,9 @@ void hdd_wlan_exit(hdd_adapter_t *pAdapter)
       VOS_ASSERT( VOS_IS_STATUS_SUCCESS( vosStatus ) );
    }
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
+   hdd_unregister_mcast_bcast_filter(pAdapter);
+#endif
    //Close VOSS
    vos_close(pVosContext);
 
@@ -1300,6 +1305,9 @@ int hdd_wlan_sdio_probe(struct sdio_func *sdio_func_dev )
       goto err_nl_srv;
    }
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
+   hdd_register_mcast_bcast_filter(pAdapter);
+#endif
    //Trigger the initial scan
    hdd_wlan_initial_scan(pAdapter);
 
