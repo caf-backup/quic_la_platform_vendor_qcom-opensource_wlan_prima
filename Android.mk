e8ee218403646300894546c6985511f28402d2e8
Dominic Binks <dbinks@quicinc.com>
2011-05-19 12:27:28 -0700

    Build utils from the utils directory
    Modify the build to pick up the utils from the utils directory
    rather than from the board specific directories.  Modify the make
    files to ensure that the ftm directory symlink is made since this
    won't make it through the packing process.  This with an associated
    change in the pack rules should mean HY11 has WLAN sources that
    build
    
    Change-Id: I07db139a4c670c6df6b6fc5f9a1b55667e4158bd

--- Android.mk
+++ Android.mk
@@ -3,23 +3,23 @@ WLAN_BLD_DIR := $(call my-dir)
 #Build/Package Libra Mono only in case of 7627 target
 ifneq (, $(filter msm7627_surf msm7627_ffa, $(QCOM_TARGET_PRODUCT)))
         include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/libra/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/libra/utils/asf/src/Android.mk
+        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 #Build/Package Libra and Volans Module in case of msm7x30_surf target
 ifneq (, $(filter msm7630_surf, $(QCOM_TARGET_PRODUCT)))
         include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
         include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/libra/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/libra/utils/asf/src/Android.mk
+        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 #Build/Package Volans Module only in case of 8660 target variants
 ifeq "$(findstring msm8660,$(QCOM_TARGET_PRODUCT))" "msm8660"
         include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/libra/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/libra/utils/asf/src/Android.mk
+        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 #Build/Package Prima Module only in case of 8960 target variants
@@ -30,6 +30,6 @@ endif
 #Build/Package Volans Module only in case of 7627a target
 ifneq (, $(filter msm7627a, $(QCOM_TARGET_PRODUCT)))
         include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/libra/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/libra/utils/asf/src/Android.mk
+        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
