870809f0d4d3c56f52c1d07dee97a65c9008763c
Sunil Ravi <c_sravi@qualcomm.com>
2011-05-26 18:31:01 +0530

    wlan: Dynamic Beacon Window And Reference TBTT adjustments
    Fixed fat beacon issue.
    wlan code updated to corestack ver 3.0.0.62 and FW ver 3.0.0.35
    CRs-fixed: 278788
    
    Change-Id: I650f7e5d86cd5551a7ebcb9dee854e169b218ed1

--- libra/wlanfw/inc/qwlanfw_version.h
+++ libra/wlanfw/inc/qwlanfw_version.h
@@ -19,7 +19,7 @@ BRIEF DESCRIPTION:
 #define WLANFW_VERSION_MINOR            0
 #define WLANFW_VERSION_PATCH            0
 #define WLANFW_VERSION_EXTRA            ""
-#define WLANFW_VERSION_BUILD            30
+#define WLANFW_VERSION_BUILD            35
 
 
 #define WLANFW_VERSION \
@@ -28,7 +28,7 @@ BRIEF DESCRIPTION:
         WLANFW_VERSION_MINOR, \
         WLANFW_VERSION_PATCH)
 
-#define WLANFW_VERSIONSTR               "3.0.0.30"
+#define WLANFW_VERSIONSTR               "3.0.0.35"
 
 /*---------------------------------------------------------------------------
  * Encoding of version information
