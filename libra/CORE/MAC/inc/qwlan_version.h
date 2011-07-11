fa23382f4b8fc1cf47ea7c9806a7f1a373590766
Sunil Ravi <c_sravi@qualcomm.com>
2011-07-11 17:10:47 +0530

    wlan: Fixed Multiple SSID shows up from one AP
    Changed frame parser code to detect duplicate SSID
    and not to overwrite the first SSID found. This is to deal with some mal-formed beacon.
    
    Change-Id: I973e282c7fbc51c4fa912838add57dbf084ebc68
    CRs-fixed: 276704

--- libra/CORE/MAC/inc/qwlan_version.h
+++ libra/CORE/MAC/inc/qwlan_version.h
@@ -19,8 +19,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            67
+#define QWLAN_VERSION_BUILD            68
 
-#define QWLAN_VERSIONSTR               "3.0.0.67"
+#define QWLAN_VERSIONSTR               "3.0.0.68"
 
 #endif /* QWLAN_VERSION_H */
