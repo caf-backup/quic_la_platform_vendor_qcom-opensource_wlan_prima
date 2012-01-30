132c229e77797d5b65bd41097b0e8a4d8dd483a3
Prasanth Chukka <c_pchukk@qualcomm.com>
2012-01-30 12:20:01 +0530

    wlan: update with correct cfg.dat for WCN1312.
    The cfg.dat was reverted in the
    gerrit id: Ia6d2a74e7b02e653e001353c7b5b248f07882433
    Updating the new cfg.dat.
    
    611646: Fixed VOS ASSERT in hdd_roamRegisterSTA Line 801 while running monkey stress test
    
    Change-Id: I57bbc4b51623b9581b6428c0acbe5a564eac87ad
    CRs-Fixed: 330475

--- libra/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ libra/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -2033,7 +2033,6 @@ static int wlan_hdd_cfg80211_disconnect( struct wiphy *wiphy,
                     reasonCode = eCSR_DISCONNECT_REASON_UNSPECIFIED;
                     break;
             }
-            pAdapter->conn_info.connState = eConnectionState_NotConnected;
             INIT_COMPLETION(pAdapter->disconnect_comp_var);
 
             /*issue disconnect*/
