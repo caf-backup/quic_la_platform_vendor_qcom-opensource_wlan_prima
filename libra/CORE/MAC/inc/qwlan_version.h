b3e272427d71674eb35704d0fa5f7381425ebf77
Kamath Vinayak <c_vkamat@qualcomm.com>
2012-02-04 20:06:59 +0530

    wlan: ES3 preliminary release #3 for WCN1312.
    Integrate the following CLs from Perforce:
    
    610470: Added a FATAL error print.
    612202: FW Reset the firmware state to BMPS state while returning
            failure for suspend_bmps message to host.
    
    Change-Id: Id754fb175a5cceb429ff3103393be80d1f645d27
    CRs-Fixed: 330156, 292779.

--- libra/CORE/MAC/inc/qwlan_version.h
+++ libra/CORE/MAC/inc/qwlan_version.h
@@ -19,8 +19,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            79
+#define QWLAN_VERSION_BUILD            81
 
-#define QWLAN_VERSIONSTR               "3.0.0.79"
+#define QWLAN_VERSIONSTR               "3.0.0.81"
 
 #endif /* QWLAN_VERSION_H */
