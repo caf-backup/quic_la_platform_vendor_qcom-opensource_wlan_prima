981fb34e9960380dd7689f295c1a2c76b044dea9
Jeff Johnson <jjohnson@qca.qualcomm.com>
2012-04-24 14:20:00 -0700

    wlan: Convert VOS global context to static
    When WLAN module is initialized, it allocates memory for VOS global
    context. Whenever WLAN is disabled/enabled this memory is allocated
    and freed. This results in un-necessary fragmented memory.
    VOS global context requires ~64K. So instead of allocating this from
    slab, make this context a static.
    
    Change-Id: I9c645b2ca808566b652b9a46f213fac83411cffc
    CRs-Fixed: 353608

--- prima/CORE/VOSS/src/vos_api.c
+++ prima/CORE/VOSS/src/vos_api.c
@@ -84,6 +84,7 @@
 /*---------------------------------------------------------------------------
  * Data definitions
  * ------------------------------------------------------------------------*/
+static VosContextType  gVosContext;
 static pVosContextType gpVosContext;
 
 /*---------------------------------------------------------------------------
@@ -132,7 +133,7 @@ VOS_STATUS vos_preOpen ( v_CONTEXT_t *pVosContext )
 
    /* Allocate the VOS Context */
    *pVosContext = NULL;
-   gpVosContext = (VosContextType*) kmalloc(sizeof(VosContextType), GFP_KERNEL);
+   gpVosContext = &gVosContext;
 
    if (NULL == gpVosContext)
    {
@@ -186,10 +187,6 @@ VOS_STATUS vos_preClose( v_CONTEXT_t *pVosContext )
                 "%s: Context mismatch", __func__);
       return VOS_STATUS_E_FAILURE;
    }
-
-   /* Free the VOS Context */
-   if(gpVosContext != NULL)
-      kfree(gpVosContext);
 
    *pVosContext = gpVosContext = NULL;
 
