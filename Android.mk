3f8697cef0818b8aaf0547a444aa3cfef9798714
Pervinde <pervinde@qualcomm.com>
2012-07-14 18:15:13 +0530

    wlan: Disabled Libra driver for JellyBean.
    Change-Id: I593db9b92fded77116a58a495e37726305cbbc87

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ WLAN_BLD_DIR := $(call my-dir)
 ifeq ($(BOARD_HAS_QCOM_WLAN), true)
 #Build/Package Libra Mono only in case of 7627 target
 ifeq ($(call is-chipset-in-board-platform,msm7627),true)
-        include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
+#       include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
         include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
         include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
@@ -11,7 +11,7 @@ endif
 #Build/Package Libra and Volans Module in case of msm7x30_surf target
 ifeq ($(call is-board-platform,msm7630_surf),true)
         include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
+#       include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
         include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
         include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
