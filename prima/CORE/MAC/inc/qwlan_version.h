8bd831c8481436007dd981cbd062d9314e4fd04a
Mohit Khanna <mkhanna@qca.qualcomm.com>
2012-07-31 13:16:40 -0700

    wlan: CS Release 2.0.5
    The following changes were integrated
    Change 640857 on 2012/07/23 by jzmuda 'Extending the fix in CL 640623 '
    Change 640868 on 2012/07/23 by etsou '[Coex WLAN host] Coex tunables '
    Change 640899 on 2012/07/24 by jjohnson 'Enhance debugging of some
    cfg80'
    Change 641019 on 2012/07/24 by pjayam 'Concurrency specific Associatio'
    Change 641199 on 2012/07/26 by c_sunilr 'Fix for system hang when Auth
    f'
    Change 641382 on 2012/07/27 by schang 'Full power mode RSSI update pro'
    Change 641384 on 2012/07/27 by mkalikot 'Fix for Memory leak after a
    WDA'
    Change 641388 on 2012/07/27 by mkalikot 'Proper error handling for
    wdaPo'
    Change 641571 on 2012/07/30 by sameert 'Block module exit until SSR is '
    
    Change-Id: I919601edaec4ba196d56eef7826029ed7fd24f53

--- prima/CORE/MAC/inc/qwlan_version.h
+++ prima/CORE/MAC/inc/qwlan_version.h
@@ -25,8 +25,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            4
+#define QWLAN_VERSION_BUILD            5
 
-#define QWLAN_VERSIONSTR               "3.2.0.4"
+#define QWLAN_VERSIONSTR               "3.2.0.5"
 
 #endif /* QWLAN_VERSION_H */
