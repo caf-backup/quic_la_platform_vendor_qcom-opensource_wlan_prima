5db15502138bffa871f62546288f888444df35db
Mohit Khanna <mkhanna@qca.qualcomm.com>
2012-08-17 13:54:53 -0700

    wlan: Fix race condition in cfg80211 scan api
    The 'scan pending' flag is reset before invoking the kernel API. So
    there is a possible race condition where in the next scan request could
    be invoked before invoking the Kernel API; and that would overwrite
    the scan request stored in the adapter. The fix is to reset the flag,
    only after invoking Kernel API.
    
    Fixed some lineendings / space issues in other files.
    
    Change-Id: I9d20f2e456ca231b3f9aaaa28f5af0ee8ba5664d
    CRs-Fixed: 383267

--- prima/CORE/SME/src/csr/csrApiRoam.c
+++ prima/CORE/SME/src/csr/csrApiRoam.c
@@ -3013,15 +3013,15 @@ eHalStatus csrRoamSetBssConfigCfg(tpAniSirGlobal pMac, tANI_U32 sessionId, tCsrR
     if(cfgCb > 2 )
     {
 	if(!WDA_getFwWlanFeatCaps(DOT11AC)) {
-	    cfgCb = csrGetHTCBStateFromVHTCBState((tAniCBSecondaryMode)pMac->roam.configParam.channelBondingMode5GHz);
-	    ccmCfgSetInt(pMac, WNI_CFG_CHANNEL_BONDING_MODE, cfgCb, NULL, eANI_BOOLEAN_FALSE);
+		cfgCb = csrGetHTCBStateFromVHTCBState((tAniCBSecondaryMode)pMac->roam.configParam.channelBondingMode5GHz);
+		ccmCfgSetInt(pMac, WNI_CFG_CHANNEL_BONDING_MODE, cfgCb, NULL, eANI_BOOLEAN_FALSE);
 	}
 	else 
 	{
-            ccmCfgSetInt(pMac, WNI_CFG_CHANNEL_BONDING_MODE, WNI_CFG_CHANNEL_BONDING_MODE_ENABLE, NULL, eANI_BOOLEAN_FALSE);
-            ccmCfgSetInt(pMac, WNI_CFG_CB_SECONDARY_CHANNEL_STATE, cfgCb, NULL, eANI_BOOLEAN_FALSE);
-            ccmCfgSetInt(pMac, WNI_CFG_VHT_CHANNEL_WIDTH,  pMac->roam.configParam.nVhtChannelWidth, NULL, eANI_BOOLEAN_FALSE);
-        }
+		ccmCfgSetInt(pMac, WNI_CFG_CHANNEL_BONDING_MODE, WNI_CFG_CHANNEL_BONDING_MODE_ENABLE, NULL, eANI_BOOLEAN_FALSE);
+		ccmCfgSetInt(pMac, WNI_CFG_CB_SECONDARY_CHANNEL_STATE, cfgCb, NULL, eANI_BOOLEAN_FALSE);
+		ccmCfgSetInt(pMac, WNI_CFG_VHT_CHANNEL_WIDTH,  pMac->roam.configParam.nVhtChannelWidth, NULL, eANI_BOOLEAN_FALSE);
+	}
     }
     else
 #endif
