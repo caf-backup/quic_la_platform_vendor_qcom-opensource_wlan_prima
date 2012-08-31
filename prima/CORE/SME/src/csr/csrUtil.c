20c1118a2db7d1ce0c7666773558a3ec84fcd41a
Mohit Khanna <mkhanna@qca.qualcomm.com>
2012-08-30 17:45:10 -0700

    wlan: Resolve build error when we disable 11AC feature in the Kbuild
    Change-Id: I87e5023f45041c591c74955f5372b8cd8782dac5

--- prima/CORE/SME/src/csr/csrUtil.c
+++ prima/CORE/SME/src/csr/csrUtil.c
@@ -2388,11 +2388,13 @@ tANI_BOOLEAN csrGetPhyModeInUse( eCsrPhyMode phyModeIn, eCsrPhyMode bssPhyMode, 
 
     if ( fMatch && pCfgDot11ModeToUse )
     {
+#ifdef WLAN_FEATURE_11AC
         if(cfgDot11Mode == eCSR_CFG_DOT11_MODE_11AC && !WDA_getFwWlanFeatCaps(DOT11AC))
         {
             *pCfgDot11ModeToUse = eCSR_CFG_DOT11_MODE_11N;
         }
         else
+#endif
         {
             *pCfgDot11ModeToUse = cfgDot11Mode;
         }
