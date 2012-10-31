0e8f061799905ef04aec1931910d26b63407f5f2
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-10-30 17:44:43 -0700

    wlan: KW Error Fixes for SME/MAC/SYS/WDA
    Change-Id: I0d84f67aee0dd08b483f38e1849659d0c8edcd40
    CR-Fixed: 414851

--- prima/CORE/WDA/src/wlan_qct_wda.c
+++ prima/CORE/WDA/src/wlan_qct_wda.c
@@ -7538,6 +7538,7 @@ VOS_STATUS WDA_ProcessHostOffloadReq(tWDA_CbContext *pWDA,
                            "%s:wdaWdiApiMsgParam is not NULL", __FUNCTION__); 
       VOS_ASSERT(0);
       vos_mem_free(wdiHostOffloadInfo);
+      vos_mem_free(pWdaParams) ;
       return VOS_STATUS_E_FAILURE;
    }
    /* Store param pointer as passed in by caller */
@@ -11651,6 +11652,12 @@ VOS_STATUS WDA_shutdown(v_PVOID_t pVosContext, wpt_boolean closeTransport)
 void WDA_stopFailed(v_PVOID_t pVosContext)
 {
    tWDA_CbContext *pWDA = (tWDA_CbContext *)VOS_GET_WDA_CTXT(pVosContext);
+   if(pWDA == NULL)
+   {
+       VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_ERROR,
+                         "Could not get the WDA Context pointer" );
+       return;
+   }
    pWDA->needShutdown  = TRUE;
 }
 /*
@@ -11661,7 +11668,13 @@ void WDA_stopFailed(v_PVOID_t pVosContext)
 v_BOOL_t WDA_needShutdown(v_PVOID_t pVosContext)
 {
    tWDA_CbContext *pWDA = (tWDA_CbContext *)VOS_GET_WDA_CTXT(pVosContext);
-   return pWDA->needShutdown;   
+   if(pWDA == NULL)
+   {
+       VOS_TRACE( VOS_MODULE_ID_WDA, VOS_TRACE_LEVEL_ERROR,
+                         "Could not get the WDA Context pointer" );
+       return 0;
+   }
+   return pWDA->needShutdown;
 }
 
 #ifdef WLAN_FEATURE_11AC
