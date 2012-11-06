2a933a282377e55f09b4a2946cfc843c4f98825a
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-11-06 15:07:28 -0800

    wlan: DUT P2P client sends disassoc frames at connected STA channels.
    When we send disassoc, from MLM we check is there any offchannel
    session, if present we send a suspend link(INIT_SCAN) to RIVA.
    Suspend internally calls mcc_suspend and if mcc is running, we
    stop the scheduler and enable all the selfsta.This causes the issue.
    If we are parked on the other channel and if we enable the transmission
    of selfsta then disassoc can go in the other channel.
    We don't need to suspend at the time of deleting the bss. So removed
    this logic from code.
    
    Change-Id: I5b3c4b5e8f7de13734d11b7d5bd54a9c0a87149f
    CR-Fixed: 416741

--- prima/CORE/MAC/src/pe/lim/limSendSmeRspMessages.c
+++ prima/CORE/MAC/src/pe/lim/limSendSmeRspMessages.c
@@ -1008,7 +1008,7 @@ limSendSmeAuthRsp(tpAniSirGlobal pMac,
 } /*** end limSendSmeAuthRsp() ***/
 
 
-void limSendSmeDisassocDeauthNtfPostResume( tpAniSirGlobal pMac,
+void limSendSmeDisassocDeauthNtf( tpAniSirGlobal pMac,
                                 eHalStatus status, tANI_U32 *pCtx )
 {
     tSirMsgQ                mmhMsg;
@@ -1201,22 +1201,8 @@ limSendSmeDisassocNtf(tpAniSirGlobal pMac,
         peDeleteSession(pMac,psessionEntry);
     }
         
-    if( IS_MCC_SUPPORTED && limIsLinkSuspended( pMac ) )
-    {
-        //Resume on the first active session channel.
-        tANI_U8 resumeChannel;
-        ePhyChanBondState resumePhyCbState;
-        peGetActiveSessionChannel( pMac, &resumeChannel, &resumePhyCbState );
-        peSetResumeChannel( pMac, resumeChannel, resumePhyCbState );
-
-        limResumeLink( pMac, limSendSmeDisassocDeauthNtfPostResume, 
+    limSendSmeDisassocDeauthNtf( pMac, eHAL_STATUS_SUCCESS,
                                               (tANI_U32*) pMsg );
-    }
-    else
-    {
-        limSendSmeDisassocDeauthNtfPostResume( pMac, eHAL_STATUS_SUCCESS,
-                                              (tANI_U32*) pMsg );
-    }
 } /*** end limSendSmeDisassocNtf() ***/
 
 
@@ -1550,22 +1536,9 @@ limSendSmeDeauthNtf(tpAniSirGlobal pMac, tSirMacAddr peerMacAddr, tSirResultCode
         peDeleteSession(pMac,psessionEntry);
     }   
 
-    if( IS_MCC_SUPPORTED && limIsLinkSuspended( pMac ) )
-    {
-        //Resume on the first active session channel.
-        tANI_U8 resumeChannel;
-        ePhyChanBondState resumePhyCbState;
-        peGetActiveSessionChannel( pMac, &resumeChannel, &resumePhyCbState );
-        peSetResumeChannel( pMac, resumeChannel, resumePhyCbState );
+    limSendSmeDisassocDeauthNtf( pMac, eHAL_STATUS_SUCCESS,
+                                              (tANI_U32*) pMsg );
 
-        limResumeLink( pMac, limSendSmeDisassocDeauthNtfPostResume, 
-                                              (tANI_U32*) pMsg );
-    }
-    else
-    {
-        limSendSmeDisassocDeauthNtfPostResume( pMac, eHAL_STATUS_SUCCESS,
-                                              (tANI_U32*) pMsg );
-    }
 } /*** end limSendSmeDeauthNtf() ***/
 
 
