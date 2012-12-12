fe294bef20e15cb52c7e0f4160231bce2920fc48
Ng Chilam <chilamng@qca.qualcomm.com>
2012-12-12 14:49:46 -0800

    wlan: Add QCA copyright information in Tdls files newly added.
    Update TDLS files with QCA copyright information.
    
    Change-Id: I6c3f5eb31af192f439a9141626c43e64d2af02da
    CR-Fixed: 426223

---
+++ prima/CORE/HDD/inc/wlan_hdd_tdls.h
@@ -0,0 +1,20 @@
+#ifndef __HDD_TDSL_H
+#define __HDD_TDSL_H
+/**===========================================================================
+
+\file         wlan_hdd_tdls.h
+
+\brief       Linux HDD TDLS include file
+
+Copyright (c) 2012-2013 Qualcomm Atheros, Inc.
+All Rights Reserved.
+Qualcomm Atheros Confidential and Proprietary.
+==========================================================================*/
+
+#define MAX_NUM_TDLS_PEER    4
+
+extern int wlan_hdd_saveTdlsPeer(tCsrRoamInfo *pRoamInfo);
+
+extern int wlan_hdd_findTdlsPeer(tSirMacAddr peerMac);
+
+#endif // __HDD_TDSL_H
