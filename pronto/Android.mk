171c7b7bd31a7709196c698ca816eb479779f0ba
Jeff Johnson <jjohnson@qca.qualcomm.com>
2013-01-07 13:05:03 -0800

    wlan: Allow both proprietary and open source pronto drivers
    Update the proprietary pronto makefile so that it can co-exist with
    the open source pronto code.  The assumption is that the open source
    makefiles will handle all firmware issues, so the proprietary makefile
    only needs to build the proprietary wlan module.
    
    Change-Id: I24a61f4158006fdd0db54fc6ad16b62eec0c123e

--- pronto/Android.mk
+++ pronto/Android.mk
@@ -8,31 +8,7 @@ else
        DLKM_DIR := build/dlkm
 endif
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := WCNSS_qcom_wlan_nv.bin
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(PRODUCT_OUT)/persist
-LOCAL_SRC_FILES    := firmware_bin/$(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := WCNSS_cfg.dat
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/firmware/wlan/prima
-LOCAL_SRC_FILES    := firmware_bin/$(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := WCNSS_qcom_cfg.ini
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(PRODUCT_OUT)/persist
-LOCAL_SRC_FILES    := firmware_bin/$(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-# Build pronto_wlan.ko
+# Build proprietary_pronto_wlan.ko
 ###########################################################
 # This is set once per LOCAL_PATH, not per (kernel) module
 KBUILD_OPTIONS := WLAN_PRONTO=../$(WLAN_BLD_DIR)/pronto
@@ -42,7 +18,7 @@ KBUILD_OPTIONS += MODNAME=wlan
 KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE              := pronto_wlan.ko
+LOCAL_MODULE              := proprietary_pronto_wlan.ko
 LOCAL_MODULE_KBUILD_NAME  := wlan.ko
 LOCAL_MODULE_TAGS         := debug
 LOCAL_MODULE_DEBUG_ENABLE := true
@@ -50,13 +26,3 @@ LOCAL_MODULE_PATH         := $(TARGET_OUT)/lib/modules/pronto
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
 
-#Create symbolic link
-$(shell mkdir -p $(TARGET_OUT)/lib/modules; \
-        ln -sf /system/lib/modules/pronto/pronto_wlan.ko \
-               $(TARGET_OUT)/lib/modules/wlan.ko)
-
-$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
-        ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
-        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
-        ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
