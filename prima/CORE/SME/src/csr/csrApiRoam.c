1215d28bbf5fca6406e2b48d3dad10bb78ce19c5
Kamath Vinayak <c_vkamat@qti.qualcomm.com>
2013-03-07 00:06:34 +0530

    wlan: sme: remove obsolete "WLAN_FEATURE_P2P" featurization
    The current driver has evolved over time from a driver that originally
    only supported "station" mode.  When the initial support for "P2P"
    mode was added, the code was added with conditional compilation so
    that the "P2P" feature could be included or excluded based upon
    the target needs.  Now, however, there is an expectation that all
    drivers will support both "station" mode and "P2P" mode.
    Therefore remove the conditional compilation since "P2P" mode code
    must always be present.
    
    Change-Id: I41d3df88da4ae459ffb6d1acf72766b4c863aa9c
    CRs-fixed: 459417

--- prima/CORE/SME/src/csr/csrApiRoam.c
+++ prima/CORE/SME/src/csr/csrApiRoam.c
@@ -10349,7 +10349,6 @@ static void csrRoamGetBssStartParms( tpAniSirGlobal pMac, tCsrRoamProfile *pProf
     
     cfgDot11Mode = csrRoamGetPhyModeBandForBss( pMac, pProfile, operationChannel, &eBand );
     
-#ifdef WLAN_FEATURE_P2P
     if( ( (pProfile->csrPersona == VOS_P2P_CLIENT_MODE) ||
           (pProfile->csrPersona == VOS_P2P_GO_MODE) )
      && ( cfgDot11Mode == eCSR_CFG_DOT11_MODE_11B)
@@ -10361,7 +10360,6 @@ static void csrRoamGetBssStartParms( tpAniSirGlobal pMac, tCsrRoamProfile *pProf
               pProfile->csrPersona);
         VOS_ASSERT(0);
     }
-#endif
     switch( cfgDot11Mode )
     {
         case eCSR_CFG_DOT11_MODE_11G:
@@ -10449,7 +10447,6 @@ static void csrRoamGetBssStartParms( tpAniSirGlobal pMac, tCsrRoamProfile *pProf
             
             break;     
         case eSIR_11G_NW_TYPE:
-#ifdef WLAN_FEATURE_P2P
             /* For P2P Client and P2P GO, disable 11b rates */ 
             if( (pProfile->csrPersona == VOS_P2P_CLIENT_MODE) ||
                 (pProfile->csrPersona == VOS_P2P_GO_MODE)
@@ -10467,7 +10464,6 @@ static void csrRoamGetBssStartParms( tpAniSirGlobal pMac, tCsrRoamProfile *pProf
                 pParam->operationalRateSet.rate[7] = SIR_MAC_RATE_54;
             }
             else
-#endif            
             {
             pParam->operationalRateSet.numRates = 4;
             pParam->operationalRateSet.rate[0] = SIR_MAC_RATE_1 | CSR_DOT11_BASIC_RATE_MASK;
