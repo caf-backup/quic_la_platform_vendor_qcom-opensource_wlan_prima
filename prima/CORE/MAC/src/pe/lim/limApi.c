c62be15976fedfa41e9dd21ea6144800115eb98f
Viral Modi <viralm@qca.qualcomm.com>
2013-03-06 02:25:31 -0800

    wlan: Decouple insert NOA from suspend link
    Suspend link in case of P2P GO is not working since as part of decoupling
    NOA from Init scan req, we did not take care of suspend link which also
    uses Init scan path (in LIM). This will cause no NOA to happen during
    suspend link used for remain on channel, innav and Join (on other channel
    for session 2) requests from SME.
    The fix is to decouple "Insert NOA" from suspend link. We will hold the
    SME requests using suspend link i.e. when there is an existing P2P GO
    session in lim Global and send down an INSERT NOA msg to FW. No states
    will be changed at LIM, TL or in FW. Once NOA starts, FW will send an
    indication back to Host that NOA started at which point host will send
    down the held SME request down to FW.
    
    Change-Id: Icdba8ff9dfe77a48f559ce5291ca97fca1e7b653
    CRs-Fixed: 456650

--- prima/CORE/MAC/src/pe/lim/limApi.c
+++ prima/CORE/MAC/src/pe/lim/limApi.c
@@ -76,7 +76,8 @@ static void __limInitScanVars(tpAniSirGlobal pMac)
 
     pMac->lim.gLimCurrentScanChannelId = 0;
     pMac->lim.gpLimMlmScanReq = NULL;
-    pMac->lim.gpLimSmeScanReq = NULL;
+    pMac->lim.gDeferMsgTypeForNOA = 0;
+    pMac->lim.gpDefdSmeMsgForNOA = NULL;
     pMac->lim.gLimMlmScanResultLength = 0;
     pMac->lim.gLimSmeScanResultLength = 0;
 
@@ -846,10 +847,10 @@ limCleanup(tpAniSirGlobal pMac)
         pMac->lim.gpLimMlmRemoveKeyReq = NULL;
     }
 
-    if (pMac->lim.gpLimSmeScanReq != NULL)
+    if (pMac->lim.gpDefdSmeMsgForNOA != NULL)
     {
-        palFreeMemory(pMac->hHdd, pMac->lim.gpLimSmeScanReq);
-        pMac->lim.gpLimSmeScanReq = NULL;
+        palFreeMemory(pMac->hHdd, pMac->lim.gpDefdSmeMsgForNOA);
+        pMac->lim.gpDefdSmeMsgForNOA = NULL;
     }
 
     if (pMac->lim.gpLimMlmScanReq != NULL)
