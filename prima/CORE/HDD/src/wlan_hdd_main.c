c708023e27318ab3a16efee62c5ed1e9fce31d46
Amar Singhal <asinghal@qca.qualcomm.com>
2013-03-21 16:05:26 -0700

    wlan: Unregister wiphy earlier
    When the driver is unloaded, unregister cfg80211 callbacks before the
    driver adapter is freed. This would prevent cfg80211 module calling
    the driver with invalid driver adapter.
    
    Change-Id: I1f131058736704a1767d396abd1a45a5ce0f5a73
    CRs-fixed: 464717

--- prima/CORE/HDD/src/wlan_hdd_main.c
+++ prima/CORE/HDD/src/wlan_hdd_main.c
@@ -3213,6 +3213,8 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
       }
    }
 
+   wiphy_unregister(wiphy);
+
    if (VOS_FTM_MODE == hdd_get_conparam())
    {
       wlan_hdd_ftm_close(pHddCtx);
@@ -3409,8 +3411,7 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
    }
 
 free_hdd_ctx:
-   wiphy_unregister(wiphy) ;
-   wiphy_free(wiphy) ;
+   wiphy_free(wiphy);
    if (hdd_is_ssr_required())
    {
        /* WDI timeout had happened during unload, so SSR is needed here */
