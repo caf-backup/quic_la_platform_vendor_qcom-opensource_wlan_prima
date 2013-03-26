8b7a791e6e50cc431007507150e3ecd5cfe81ba9
Amar Singhal <asinghal@qca.qualcomm.com>
2013-03-26 09:21:45 -0700

    Revert "wlan: Unregister wiphy earlier"
    This reverts commit c708023e27318ab3a16efee62c5ed1e9fce31d46.

--- prima/CORE/HDD/src/wlan_hdd_main.c
+++ prima/CORE/HDD/src/wlan_hdd_main.c
@@ -3213,8 +3213,6 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
       }
    }
 
-   wiphy_unregister(wiphy);
-
    if (VOS_FTM_MODE == hdd_get_conparam())
    {
       wlan_hdd_ftm_close(pHddCtx);
@@ -3411,7 +3409,8 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
    }
 
 free_hdd_ctx:
-   wiphy_free(wiphy);
+   wiphy_unregister(wiphy) ;
+   wiphy_free(wiphy) ;
    if (hdd_is_ssr_required())
    {
        /* WDI timeout had happened during unload, so SSR is needed here */
