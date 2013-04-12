52b479098d288e09be2ff615790f5a6dbced3b3a
Prasanna Kumar <kumarpra@qca.qualcomm.com>
2013-04-11 20:05:21 -0700

    feature: Implement embedded template in NV
    This feature includes embedded template in NV and allows us to add
    new parameters easily for customization. NV parser looks for a
    magic number embedded in NV, to determine it is in new format.
    Once it determines it is in new format, it parses the template and
    compare the builtIn template, it copies the data only bulitIn
    understands rest of the day is ignored.
    If we do not see magic Number in the NV, then it parses as normal
    processing as before for backward compatiblity. During cold-boot the
    NV is send to the firmware without parsing, NV parse is done in
    firmware during cold-boot as well as when we do WLAN/Wifi on.
    Add new PTT api's to get NV.bin, set NV.bin, get dictionary for tools.
    
    CRs-fixed: 474382
    Change-Id: I5726852d30629a80ec1790eff11e73b8f7e0004f

---
+++ prima/CORE/VOSS/inc/wlan_nv_parser.h
@@ -0,0 +1,14 @@
+/*
+* Copyright (c) 2013 Qualcomm Atheros, Inc.
+* All Rights Reserved.
+* Qualcomm Atheros Confidential and Proprietary.
+*/
+
+#if !defined __WLAN_NV_PARSER_H
+#define __WLAN_NV_PARSER_H
+
+#include <vos_status.h>
+
+VOS_STATUS nvParser(tANI_U8 *pnvEncodedBuf, tANI_U32 nvReadBufSize, sHalNv *);
+
+#endif
