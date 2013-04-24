f8f0726da705d4f4e6a92bdfc67fbe27bfde88b8
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-04-23 17:46:06 -0700

    wlan: Updating Android.mk file for utils dir
    There is a seperate utils project now for wlan,so removing
    the compilation for utils directory in wlan-noship directory
    
    Change-Id: Id81c4a8834bec83e6123c373664c765d83f75429

--- Android.mk
+++ Android.mk
@@ -2,53 +2,53 @@ WLAN_BLD_DIR := $(call my-dir)
 
 ifeq ($(BOARD_HAS_QCOM_WLAN), true)
 #Build/Package Libra Mono only in case of 7627 target
-ifeq ($(call is-chipset-in-board-platform,msm7627),true)
-#       include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
-endif
+#ifeq ($(call is-chipset-in-board-platform,msm7627),true)
+#        include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+#endif
 
 #Build/Package Libra and Volans Module in case of msm7x30_surf target
 ifeq ($(call is-board-platform,msm7630_surf),true)
         include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
 #       include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 #Build/Package Volans Module only in case of 8660 target variants
 ifeq ($(call is-board-platform,msm8660),true)
         include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 #Build/Package Prima Module only in case of 8960 target variants
 ifeq ($(call is-board-platform,msm8960),true)
         include $(WLAN_BLD_DIR)/prima/Android.mk
-        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 #Build/Package Pronto Module in case of 8974 target variants
 ifeq ($(call is-board-platform,msm8974),true)
         include $(WLAN_BLD_DIR)/pronto/Android.mk
-        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 #Build/Package Pronto Module in case of 8226, 8610 target variants
 ifeq ($(call is-board-platform-in-list,msm8226 msm8610),true)
         include $(WLAN_BLD_DIR)/pronto/Android.mk
-        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 #Build/Package Volans Module only in case of 7627a target
 ifeq ($(call is-board-platform,msm7627a),true)
         include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
-        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
-        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
 endif
 
 endif
