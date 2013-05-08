ea17eff41232a372c0ba19182a1e0331ee969343
Chet Lanctot <clanctot@qca.qualcomm.com>
2013-05-08 13:59:56 -0700

    wlan: Fix problems with RSN OUI used in PMF
    There are two fixes for the new RSN OUI definition for
    PSK SHA256.  One fix is so that the array index used for
    the new definition does not accidently modify the array
    index for the existing CCKM definition.  The other fix is
    so that PSK SHA256 is not taken as a match for either RSN
    or PSK since they are different AKM suites and should not
    match PSK SHA256.
    
    Change-Id: I5523280a2bc5f56e8895325aab5300e5bf531c99
    CRs-Fixed: 452558

--- prima/CORE/SME/src/csr/csrUtil.c
+++ prima/CORE/SME/src/csr/csrUtil.c
@@ -50,13 +50,8 @@ tANI_U8 csrRSNOui[][ CSR_RSN_OUI_SIZE ] = {
     { 0x00, 0x0F, 0xAC, 0x03 }, // Reserved
     { 0x00, 0x0F, 0xAC, 0x04 }, // AES-CCMP
     { 0x00, 0x0F, 0xAC, 0x05 }, // WEP-104
-#ifdef WLAN_FEATURE_11W
-    { 0x00, 0x0F, 0xAC, 0x06 },  // BIP(encryption type) or (RSN-PSK-SHA256(authentication type)
-#endif
-#ifdef FEATURE_WLAN_CCX
-    { 0x00, 0x40, 0x96, 0x00 } // CCKM
-#endif /* FEATURE_WLAN_CCX */
-    
+    { 0x00, 0x40, 0x96, 0x00 }, // CCKM
+    { 0x00, 0x0F, 0xAC, 0x06 }  // BIP (encryption type) or RSN-PSK-SHA256 (authentication type)
 };
 
 #ifdef FEATURE_WLAN_WAPI
@@ -3349,23 +3344,13 @@ static tANI_BOOLEAN csrIsAuthRSN( tpAniSirGlobal pMac, tANI_U8 AllSuites[][CSR_R
                                   tANI_U8 cAllSuites,
                                   tANI_U8 Oui[] )
 {
-#ifdef WLAN_FEATURE_11W
-    return( csrIsOuiMatch( pMac, AllSuites, cAllSuites, csrRSNOui[01], Oui ) ||
-            csrIsOuiMatch( pMac, AllSuites, cAllSuites, csrRSNOui[06], Oui ));
-#else
     return( csrIsOuiMatch( pMac, AllSuites, cAllSuites, csrRSNOui[01], Oui ) );
-#endif
 }
 static tANI_BOOLEAN csrIsAuthRSNPsk( tpAniSirGlobal pMac, tANI_U8 AllSuites[][CSR_RSN_OUI_SIZE],
                                       tANI_U8 cAllSuites,
                                       tANI_U8 Oui[] )
 {
-#ifdef WLAN_FEATURE_11W
-    return( csrIsOuiMatch( pMac, AllSuites, cAllSuites, csrRSNOui[02], Oui ) ||
-            csrIsOuiMatch( pMac, AllSuites, cAllSuites, csrRSNOui[06], Oui ) );
-#else
     return( csrIsOuiMatch( pMac, AllSuites, cAllSuites, csrRSNOui[02], Oui ) );
-#endif
 }
 
 #ifdef WLAN_FEATURE_11W
@@ -3373,7 +3358,7 @@ static tANI_BOOLEAN csrIsAuthRSNPskSha256( tpAniSirGlobal pMac, tANI_U8 AllSuite
                                       tANI_U8 cAllSuites,
                                       tANI_U8 Oui[] )
 {
-    return csrIsOuiMatch( pMac, AllSuites, cAllSuites, csrRSNOui[06], Oui );
+    return csrIsOuiMatch( pMac, AllSuites, cAllSuites, csrRSNOui[07], Oui );
 }
 #endif
 
