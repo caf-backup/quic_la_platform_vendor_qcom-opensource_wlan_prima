edabe4241b5135979f319e78eb9365934efa6224
Vasanthakumar Thiagarajan <vthiagar@qti.qualcomm.com>
2013-05-27 13:18:07 +0530

    tlshim: Maintain sta specific information in tl shim
    Store information like whether the station is registered
    for txrx, station specific rx call back to HDD, etc.
    
    Change-Id: I607d951f05b8c9fb923bfa2509490ccd5bf17e25

--- cld/CORE/TLSHIM/tl_shim.h
+++ cld/CORE/TLSHIM/tl_shim.h
@@ -7,10 +7,15 @@
 #ifndef TXRX_TL_SHIM_H
 #define TXRX_TL_SHIM_H
 
+struct tlshim_sta_info {
+	bool registered;
+};
+
 struct txrx_tl_shim_ctx {
 	void *cfg_ctx;
 	ol_txrx_tx_fp tx;
 	WLANTL_MgmtFrmRxCBType mgmt_rx;
+	struct tlshim_sta_info sta_info[WLAN_MAX_STA_COUNT];
 };
 
 #endif
