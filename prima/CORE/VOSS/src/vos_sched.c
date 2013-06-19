912cb8f18cb0d8447cd5074e63451071c04a6443
Kamath Vinayak <c_vkamat@qti.qualcomm.com>
2013-06-19 19:37:38 +0530

    wlan: Fix misuse of init_completion
    calling init_completion instead of INIT_COMPLETION could cause race
    condition because init_completion() call does re-inits
    not only the variable carrying the flag that the completion finished,
    but also initializes the waitqueue associated with the completion.
    
    Change-Id: I6a24214d40c4d19c7ba3cc7f620697552d14bf46
    CRs-fixed: 501429

--- prima/CORE/VOSS/src/vos_sched.c
+++ prima/CORE/VOSS/src/vos_sched.c
@@ -1086,7 +1086,7 @@ static int VosRXThread ( void * Arg )
         /* Rx Thread Suspended */
         complete(&pHddCtx->rx_sus_event_var);
 
-        init_completion(&pSchedContext->ResumeRxEvent);
+        INIT_COMPLETION(pSchedContext->ResumeRxEvent);
         spin_unlock(&pSchedContext->RxThreadLock);
 
         /* Wait for Resume Indication */
