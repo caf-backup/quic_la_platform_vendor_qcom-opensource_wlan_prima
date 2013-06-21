7653b71d687ad678569a80915f971c7eedfbe9e7
Hema Aparna Medicharla <haparna@qti.qualcomm.com>
2013-06-21 18:35:53 +0530

    wlan: Add NULL check before accessing configBssReqParam
    Added NULL check before accessing configBssReqParam in
    WDA_ProcessConfigBssReq and before sending the SIR_HAL_ADD_BSS_REQ
    in limProcessMlmFTReassocReq to avoid NULL pointer dereference
    at race conditions. Also added log in limProcessFTPreauthRspTimeout.
    
    Change-Id: Iccb73d90e9b04e040f996c89d42dd7a437ed08d7
    CRs-fixed: 459411

--- prima/CORE/MAC/src/pe/lim/limFT.c
+++ prima/CORE/MAC/src/pe/lim/limFT.c
@@ -1079,7 +1079,11 @@ void limProcessMlmFTReassocReq(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf,
     limDiagEventReport(pMac, WLAN_PE_DIAG_REASSOCIATING, psessionEntry, 0, 0);
 #endif
 
-
+    if (NULL == pMac->ft.ftPEContext.pAddBssReq)
+    {
+        limLog(pMac, LOGE, FL("pAddBssReq is NULL"));
+        return;
+    }
     if( eHAL_STATUS_SUCCESS != palAllocateMemory( pMac->hHdd, (void **)&pMlmReassocReq,
         sizeof(tLimMlmReassocReq)))
     {
@@ -1212,6 +1216,7 @@ void limProcessFTPreauthRspTimeout(tpAniSirGlobal pMac)
 
     // We have failed pre auth. We need to resume link and get back on
     // home channel.
+    limLog(pMac, LOG1, FL("FT Pre-Auth Time Out!!!!"));
 
     if((psessionEntry = peFindSessionBySessionId(pMac, pMac->lim.limTimers.gLimFTPreAuthRspTimer.sessionId))== NULL) 
     {
