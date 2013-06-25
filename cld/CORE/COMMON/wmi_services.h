8c3d60ff9e1513882bff180ff5b70db0e77a41ef
Vasanthakumar Thiagarajan <vthiagar@qca.qualcomm.com>
2013-06-25 18:34:19 +0530

    wmi: Update WMI command/event ids
    Sync to the lates wmi cmd/event id changes.
    
    Change-Id: Ic16c2f116da3f0844e7c8c4afc9ba603b0e17d62

--- cld/CORE/COMMON/wmi_services.h
+++ cld/CORE/COMMON/wmi_services.h
@@ -30,7 +30,6 @@ typedef  enum  {
     WMI_SERVICE_PHYERR,               /* PHY error */
     WMI_SERVICE_BCN_FILTER,           /* Beacon filter support */
     WMI_SERVICE_RTT,                  /* RTT (round trip time) support */
-    WMI_SERVICE_RATECTRL,             /* Rate-control */
     WMI_SERVICE_WOW,                  /* WOW Support */
     WMI_SERVICE_ARPNS_OFFLOAD,        /* ARP NS Offload support */
     WMI_SERVICE_NLO,                  /* Network list offload service */
@@ -42,6 +41,16 @@ typedef  enum  {
     WMI_SERVICE_PACKET_POWER_SAVE,    /* packet power save service */
     WMI_SERVICE_FORCE_FW_HANG,        /* Service to test the firmware recovery mechanism */
     WMI_SERVICE_GPIO,                 /* GPIO service */
+    WMI_SERVICE_STA_DTIM_PS_MODULATED_DTIM, /* Modulated DTIM support */
+    WMI_STA_UAPSD_BASIC_AUTO_TRIG,          /* Basic version of station UAPSD AC Trigger Generation Method with
+                                             * variable tigger periods (service, delay, and suspend intervals) */
+    WMI_STA_UAPSD_VAR_AUTO_TRIG,            /* Station UAPSD AC Trigger Generation Method with variable
+                                             * trigger periods (service, delay, and suspend intervals) */
+    WMI_SERVICE_STA_KEEP_ALIVE,              /* Serivce to support the STA KEEP ALIVE mechanism */
+    WMI_SERVICE_TX_ENCAP,             /* Packet type for TX encapsulation */
+    WMI_SERVICE_AP_PS_DETECT_OUT_OF_SYNC, /* detect out-of-sync sleeping stations */
+    WMI_SERVICE_EARLY_RX,             /* adaptive early-rx feature */
+    WMI_SERVICE_STA_SMPS, /* STA MIMO-PS */
     WMI_MAX_SERVICE=64                /* max service */
 } WMI_SERVICE;
 
