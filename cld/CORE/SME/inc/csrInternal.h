57d3329e253f8276c33b3975bb36908ac47c9c34
Mushtaq A Mujale <mmujale@qca.qualcomm.com>
2013-09-25 20:32:39 -0700

    wlan: Dynamic configuration of VHT support in 2.4 GHz band
    Providing fix for allowing the user to select VHT support through
    INI file in 2.4 GHz band. By Default VHT is disable in 2.4GHz.
    This can be enabled using gEnableVhtFor24GHzBand, which is currently
    not exposed to ini.
    
    Changes taken from Pronto: https://review-android.quicinc.com/#/c/340776/7
    
    CRs-fixed: 549981
    
    Change-Id: I79183d9a74efc53d110dd691c882634b616e7584

--- cld/CORE/SME/inc/csrInternal.h
+++ cld/CORE/SME/inc/csrInternal.h
@@ -607,6 +607,7 @@ typedef struct tagCsrConfig
     tANI_U8   txBFEnable;
     tANI_U8   txBFCsnValue;
     tANI_U8   enable2x2;
+    tANI_BOOLEAN enableVhtFor24GHz;
 #endif
     tANI_U8   txLdpcEnable;
 
