0ab1fab4ec1745179decbebacbea6c3d69913f70
c_hpothu <c_hpothu@qti.qualcomm.com>
2013-10-01 19:01:57 +0530

    wlan: HDD layer logging enchancemnt phase-1.
    Added logs which should get printed when some unexpected error
    happens.In this change, updating wlan_hdd_cfg.c,wlan_hdd_main.c
    wlan_hdd_cfg80211.c and wlan_hdd_dev_pwr.c.
    
    Change-Id: Icb8cb6f241b6ab3c86647f6712103fa20a0e735c
    CRs-Fixed: 585422

--- prima/CORE/HDD/src/wlan_hdd_dev_pwr.c
+++ prima/CORE/HDD/src/wlan_hdd_dev_pwr.c
@@ -84,7 +84,7 @@ static bool suspend_notify_sent;
 ----------------------------------------------------------------------------*/
 static int wlan_suspend(hdd_context_t* pHddCtx)
 {
-   int rc = 0;
+   long rc = 0;
 
    pVosSchedContext vosSchedContext = NULL;
 
@@ -117,9 +117,11 @@ static int wlan_suspend(hdd_context_t* pHddCtx)
    /* Wait for Suspend Confirmation from Tx Thread */
    rc = wait_for_completion_interruptible_timeout(&pHddCtx->tx_sus_event_var, msecs_to_jiffies(200));
 
-   if(!rc)
+   if (rc <= 0)
    {
-      VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL, "%s: Not able to suspend TX thread timeout happened", __func__);
+      VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL,
+           "%s: Not able to suspend TX thread timeout happened %ld"
+           , __func__, rc);
       clear_bit(TX_SUSPEND_EVENT_MASK, &vosSchedContext->txEventFlag);
 
       return -ETIME;
@@ -137,9 +139,10 @@ static int wlan_suspend(hdd_context_t* pHddCtx)
    /* Wait for Suspend Confirmation from Rx Thread */
    rc = wait_for_completion_interruptible_timeout(&pHddCtx->rx_sus_event_var, msecs_to_jiffies(200));
 
-   if(!rc)
+   if (rc <= 0)
    {
-       VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL, "%s: Not able to suspend Rx thread timeout happened", __func__);
+       VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL,
+            "%s: Not able to suspend Rx thread timeout happened %ld", __func__, rc);
 
        clear_bit(RX_SUSPEND_EVENT_MASK, &vosSchedContext->rxEventFlag);
 
@@ -167,7 +170,9 @@ static int wlan_suspend(hdd_context_t* pHddCtx)
 
    if(!rc)
    {
-       VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL, "%s: Not able to suspend MC thread timeout happened", __func__);
+       VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL,
+            "%s: Not able to suspend MC thread timeout happened %ld",
+            __func__, rc);
 
        clear_bit(MC_SUSPEND_EVENT_MASK, &vosSchedContext->mcEventFlag);
 
