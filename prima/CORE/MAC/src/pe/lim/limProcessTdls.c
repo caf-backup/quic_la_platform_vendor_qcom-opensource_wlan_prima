35828c54f24e9413ed367bd55bb41b34339ff4dd
Praveen Kumar Sirisilla <c_pksiri@qca.qualcomm.com>
2013-10-10 17:20:48 -0700

    SS Code changes to pass the code review inspection [UMAC} changes
    The patch modifies UMAC code to pass the SS code review inspection
    
    Change-Id: Id527265f0e3ed33a03ab242a3e0afec069487269
    CRs-Fixed: 555476

--- prima/CORE/MAC/src/pe/lim/limProcessTdls.c
+++ prima/CORE/MAC/src/pe/lim/limProcessTdls.c
@@ -2506,6 +2506,7 @@ limTdlsPopulateMatchingRateSet(tpAniSirGlobal pMac,
     {
         /// Could not get rateset from CFG. Log error.
         limLog(pMac, LOGP, FL("could not retrieve rateset"));
+        val = 0;
     }
     tempRateSet.numRates = val;
 
