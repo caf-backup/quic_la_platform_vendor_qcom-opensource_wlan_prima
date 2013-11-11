d9051906b8a3f517ddb8db58f6fd5b4c4f04dd59
Jeff Johnson <jjohnson@qca.qualcomm.com>
2013-11-11 12:10:25 -0800

    wlan: add __printf() annotation to logging APIs
    GCC has the capability to verify that arguments match the format
    string in printf-style functions, but only if the functions are
    properly annotated.  Annotate the various logging APIs to enable this
    capability.
    
    Change-Id: I2cca0cfd3a94f13420c25808327d610e015c8c85
    CRs-fixed: 570511

--- prima/CORE/SME/inc/smsDebug.h
+++ prima/CORE/SME/inc/smsDebug.h
@@ -3,28 +3,32 @@
  * All Rights Reserved.
  * Qualcomm Atheros Confidential and Proprietary.
  */
-/** ------------------------------------------------------------------------- * 
-    ------------------------------------------------------------------------- *  
+/** ------------------------------------------------------------------------- *
+    ------------------------------------------------------------------------- *
 
-  
-    \file cssDebug.h
-  
+
+    \file smsDebug.h
+
     Define debug log interface for SMS.
-  
+
    Copyright (C) 2006 Airgo Networks, Incorporated
- 
+
    ========================================================================== */
-   
+
 #ifndef SMS_DEBUG_H__
 #define SMS_DEBUG_H__
-
-//#include <stdio.h>
-//#include <stdarg.h>
 
 #include "utilsApi.h"
 #include "sirDebug.h"
 
-void smsLog(tpAniSirGlobal pMac, tANI_U32 loglevel, const char *pString,...); 
-void pmcLog(tpAniSirGlobal pMac, tANI_U32 loglevel, const char *pString,...);
+#if !defined(__printf)
+#define __printf(a,b)
+#endif
 
-#endif // __HAL_DEBUG_H__
+void __printf(3,4)
+smsLog(tpAniSirGlobal pMac, tANI_U32 loglevel, const char *pString, ...);
+
+void __printf(3,4)
+pmcLog(tpAniSirGlobal pMac, tANI_U32 loglevel, const char *pString, ...);
+
+#endif // __SMS_DEBUG_H__
