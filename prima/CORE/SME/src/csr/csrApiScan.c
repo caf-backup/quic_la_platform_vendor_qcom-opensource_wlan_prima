5a6d695e0f70e328c80aa793310e216f24042af4
Manjunathappa Prakash <c_pmanju@qca.qualcomm.com>
2013-11-11 16:22:19 -0800

    wlan: fix compilation errors for PNO and ROAM_SCAN_OFFLOAD feature flags
    WLAN driver fails to compile once FEATURE_WLAN_SCAN_PNO and
    FEATURE_ROAM_SCAN_OFFLOAD are disabled in KBUILD.
    
    Patch takescare of reported build failures when either or both of
    features are disabled.
    
    Change-Id: Iaaf9653653ca5e0c1805c2a97709b68f8b90a699
    CRs-Fixed: 572897

--- prima/CORE/SME/src/csr/csrApiScan.c
+++ prima/CORE/SME/src/csr/csrApiScan.c
@@ -7900,6 +7900,7 @@ tANI_BOOLEAN csrRoamIsValidChannel( tpAniSirGlobal pMac, tANI_U8 channel )
     return fValid;
 }
 
+#ifdef FEATURE_WLAN_SCAN_PNO
 eHalStatus csrScanSavePreferredNetworkFound(tpAniSirGlobal pMac,
             tSirPrefNetworkFoundInd *pPrefNetworkFoundInd)
 {
@@ -8069,6 +8070,7 @@ eHalStatus csrScanSavePreferredNetworkFound(tpAniSirGlobal pMac,
 
    return eHAL_STATUS_SUCCESS;
 }
+#endif //FEATURE_WLAN_SCAN_PNO
 
 #ifdef FEATURE_WLAN_LFR
 void csrInitOccupiedChannelsList(tpAniSirGlobal pMac)
