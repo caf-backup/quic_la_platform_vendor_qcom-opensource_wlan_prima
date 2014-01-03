42f65bcccc5e6d7f7fe05eeb13d2e02127a3c6a2
Prakash Dhavali <pdhavali@qca.qualcomm.com>
2014-01-02 20:12:23 -0800

    Initial commit for forking QCACLD-NEW development to caf branch
    Initial commit for forking QCACLD-NEW development to caf branch
    Sync'd upto d44508bf1612c456d5b09192966308669fb5e70b
    (wlan: Fix to enable short slot operation in IBSS mode)
    
    Change-Id: I479043548fd68bcf18bdde885aab615ffad3b750

---
+++ qcacld-new/CORE/MAC/src/include/sysGlobal.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2013 Qualcomm Atheros, Inc.
+ * All Rights Reserved.
+ * Qualcomm Atheros Confidential and Proprietary.
+ *
+ */
+#ifndef __SYS_GLOBAL_H__
+#define __SYS_GLOBAL_H__
+
+typedef struct sAniSirSys
+{
+    tANI_U32 abort; /* system is aborting and will be unloaded, only MMH thread is running */
+
+    tANI_U32 gSysFrameCount[4][16];
+    tANI_U32 gSysBbtReceived;
+    tANI_U32 gSysBbtPostedToLim;
+    tANI_U32 gSysBbtPostedToSch;
+    tANI_U32 gSysBbtPostedToPmm;
+    tANI_U32 gSysBbtPostedToHal;
+    tANI_U32 gSysBbtDropped;
+    tANI_U32 gSysBbtNonLearnFrameInv;
+    tANI_U32 gSysBbtLearnFrameInv;
+    tANI_U32 gSysBbtCrcFail;
+    tANI_U32 gSysBbtDuplicates;
+    tANI_U32 gSysReleaseCount;
+    tANI_U32 probeError, probeBadSsid, probeIgnore, probeRespond;
+
+    tANI_U32 gSysEnableLearnMode;
+    tANI_U32 gSysEnableScanMode;
+    tANI_U32 gSysEnableLinkMonitorMode;
+} tAniSirSys, *tpAniSirSys;
+
+#endif
---
+++ qcacld-new/CORE/SYS/legacy/src/system/inc/sysStartup.h
@@ -0,0 +1,32 @@
+
+/*
+ * Copyright (c) 2011-2013 Qualcomm Atheros, Inc.
+ * All Rights Reserved.
+ * Qualcomm Atheros Confidential and Proprietary.
+ *
+ * Woodside Networks, Inc proprietary. All rights reserved
+ * sysStartup.h: System startup header file.
+ * Author:  V. K. Kandarpa
+ * Date:    01/29/2002
+ *
+ * History:-
+ * Date     Modified by         Modification Information
+ * --------------------------------------------------------------------------
+ *
+ */
+
+# ifndef __SYSSTARTUP_H
+# define __SYSSTARTUP_H
+
+#include "sirParams.h"
+
+/* Defines */
+
+/* Function */
+
+extern void sysMACCleanup(void *);
+extern tSirRetStatus sysBbtProcessMessageCore(struct sAniSirGlobal *, tpSirMsgQ,
+                                               tANI_U32, tANI_U32);
+
+
+# endif /* __SYSSTARTUP_H */
