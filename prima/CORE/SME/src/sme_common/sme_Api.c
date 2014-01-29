43e9156fc75a6271421cd9ffe5a9643201cc99d5
Agarwal Ashish <ashishka@qti.qualcomm.com>
2014-01-29 19:40:30 +0530

    wlan: flush scan results which are not in new  Country code.
    Right now scan result are getting flashed out on basis of scan
    request on that particular channel. It's wise to flush scan
    entries on channels which are not part of new channel list
    of acquired country
    
    CRs-Fixed: 607644
    Change-Id: I825fc49a390ac0395b770dfa879351ca04ddae90

--- prima/CORE/SME/src/sme_common/sme_Api.c
+++ prima/CORE/SME/src/sme_common/sme_Api.c
@@ -7253,6 +7253,12 @@ eHalStatus sme_HandleChangeCountryCode(tpAniSirGlobal pMac,  void *pMsgBuf)
 
    /* reset info based on new cc, and we are done */
    csrResetCountryInformation(pMac, eANI_BOOLEAN_TRUE, eANI_BOOLEAN_TRUE);
+   /* Country code  Changed, Purge Only scan result
+    * which does not have channel number belong to 11d
+    * channel list
+    */
+   csrScanFilter11dResult(pMac);
+
 #endif
    if( pMsg->changeCCCallback )
    {
@@ -7365,7 +7371,11 @@ eHalStatus sme_HandleChangeCountryCodeByUser(tpAniSirGlobal pMac,
         pMac->scan.f11dInfoApplied = eANI_BOOLEAN_TRUE;
         pMac->scan.f11dInfoReset = eANI_BOOLEAN_FALSE;
     }
-
+    /* Country code  Changed, Purge Only scan result
+     * which does not have channel number belong to 11d
+     * channel list
+     */
+    csrScanFilter11dResult(pMac);
     // Do active scans after the country is set by User hints or Country IE
     pMac->scan.curScanType = eSIR_ACTIVE_SCAN;
 
@@ -7425,6 +7435,11 @@ eHalStatus sme_HandleChangeCountryCodeByCore(tpAniSirGlobal pMac, tAniGenericCha
             csrInitChannelList(pMac);
         }
     }
+    /* Country code  Changed, Purge Only scan result
+     * which does not have channel number belong to 11d
+     * channel list
+     */
+    csrScanFilter11dResult(pMac);
     smsLog(pMac, LOG1, FL(" returned"));
     return eHAL_STATUS_SUCCESS;
 }
